c7db57e96583de254f0fb4d409415393754e0608
Victor Rivas <victorenrique2002@gmail.com>
2023-11-28 21:48:21 -0400

    Update about me section with correct experience duration

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -12,7 +12,7 @@ function About() {
             <div className='lg:w-1/2 lg:h-full xs:w-full xs:h-1/2 flex  justify-center items-center'>
                 <div className='flex flex-col lg:pl-10 lg:pr-20 lg:pt-4 lg:pb-0 xs:pl-14 xs:pr-14 xs:pb-10 xs:pt-4 '>
                     <h2 className='text-light-blue font-bold text-4xl pb-6'>About Me</h2>
-                    <p className='text-white lg:text-base xs:text-xs'> I have approximately 6 months of experience working remotely for various companies. During this time, I've successfully delivered projects ranging from simple web pages to complete e-commerce solutions. As a self-taught enthusiast, I'm always eager to expand my knowledge and skills.
+                    <p className='text-white lg:text-base xs:text-xs'> I have approximately 1 year of experience working remotely for various companies. During this time, I've successfully delivered projects ranging from simple web pages to complete e-commerce solutions. As a self-taught enthusiast, I'm always eager to expand my knowledge and skills.
                         Currently, I'm pursuing a degree in Systems Engineering at the Metropolitan University of Caracas,
                         now in my fourth trimester. My lifelong dream has always been to be a programmer, and
                         I'm living that dream every day. If I were to sum up my life philosophy in one sentence,
